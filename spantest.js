d64a3b899019dba09e3f972707575973db8da32d
Yoonseok Ko <mir597@gmail.com>
2015-07-22 13:28:46 +0900

    Added test cases for parser span info.

---
+++ spantest.js
@@ -0,0 +1,3 @@
+$(id).getStyle('top').toInt(); // 1-6, 1-22, 1-29
+jQuery( document ).trigger( "ready" ).off( "ready" ); // 1-19, 1-38, 1-53
+this.create({delay: delay, bind: bind, arguments: args})(); // 1-57, 1-59
