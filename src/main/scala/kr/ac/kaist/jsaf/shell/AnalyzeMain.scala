f27dd13d5f21f0b204f7e864541c5f1f483b1e65
Yoonseok Ko <mir597@gmail.com>
2015-07-23 16:23:38 +0900

    Revised code.

--- src/main/scala/kr/ac/kaist/jsaf/shell/AnalyzeMain.scala
+++ src/main/scala/kr/ac/kaist/jsaf/shell/AnalyzeMain.scala
@@ -87,37 +87,39 @@ object AnalyzeMain {
     // The file must contain all the function call histories for a given executions.
     // A function call consists of 8 integers which format is as follows:
     //  start_line_of_decl:start_column:end_line:end_column:start_line_of_callexpr:start_column:end_line_end_column
-    val json = parse(Source.fromFile(filename) mkString)
+    if (filename != null) {
+      val json = parse(Source.fromFile(filename) mkString)
 
-    json.children.foreach {
-      case JField(name, value) =>
-        value match {
-          case JArray(list) =>
-            list.foreach {
-              case JString(str) =>
-                if (str.contains("@")) {
-                  // user function call
-                  val declsite = parse_span(str, false)
-                  val callsite = parse_span(name, true)
+      json.children.foreach {
+        case JField(name, value) =>
+          value match {
+            case JArray(list) =>
+              list.foreach {
+                case JString(str) =>
+                  if (str.contains("@")) {
+                    // user function call
+                    val declsite = parse_span(str, false)
+                    val callsite = parse_span(name, true)
 
-                  val dn = find(declsite)
-                  val cn = find(callsite)
+                    val dn = find(declsite)
+                    val cn = find(callsite)
 
-                  (dn, cn) match {
-                    case (Some(d), Some(c)) => map += (d, c) -> 1
-                    case _ =>
+                    (dn, cn) match {
+                      case (Some(d), Some(c)) => map += (d, c) -> 1
+                      case _ =>
                       // offset mismatch.
+                    }
+                  } else {
+                    // TODO built-in function calls should be considered.
+                    System.err.println(name + " -> " + str)
                   }
-                } else {
-                  // TODO built-in function calls should be considered.
-                  System.err.println(name + " -> "+str)
-                }
-              case _ =>
-            }
+                case _ =>
+              }
 
-          case _ =>
-        }
-      case _ =>
+            case _ =>
+          }
+        case _ =>
+      }
     }
 
     map
@@ -141,7 +143,23 @@ object AnalyzeMain {
     val hoistedProgram = new Hoister(program).doit().asInstanceOf[Program]
     val disambiguatedProgram = new Disambiguator(hoistedProgram, disambiguateOnly = false).doit().asInstanceOf[Program]
 
-    val (decls, calls) = walkAST(null, disambiguatedProgram)(Nil, Nil)
+    // Function Decl/Expr and Callsite Collector
+    def collectDeclCallPair(parent: Any, node: Any, pair: (List[Any], List[Any])) = node match {
+      case SFunDecl(info, ftn, strict) =>
+        (node::pair._1, pair._2)
+      case SFunExpr(info, ftn) =>
+        (node::pair._1, pair._2)
+      case SNew(info, lhs) =>
+        lhs match {
+          case SFunApp(_, _, _) => pair
+          case _ => (pair._1, node::pair._2) // case for 'new A'
+        }
+      case SFunApp(info, fun, args) =>
+        (pair._1, node::pair._2)
+      case _ => pair
+    }
+
+    val (decls, calls) = walkAST(collectDeclCallPair)(null, disambiguatedProgram)(Nil, Nil)
 
     System.err.println("** Decls **")
     decls.foreach {
@@ -211,6 +229,8 @@ object AnalyzeMain {
           name(lhs)
         case SVarRef(_, id) =>
           id.getText
+        case SDot(_, obj, member) =>
+          member.getText
         case _ => ""
       }
     }
@@ -230,27 +250,25 @@ object AnalyzeMain {
       })
     }
 
-    if (Shell.params.opt_ResultFileName != null) {
-      // Parse the result.
-      val result_map = parseFromFile(decls, calls, Shell.params.opt_ResultFileName)
+    // Parse the result.
+    val result_map = parseFromFile(decls, calls, Shell.params.opt_ResultFileName)
 
-      // Initialize features.
-      val feature_map =
-        init_set(init_map) >>
-          exprClassFeature >>
-          nameFeature
+    // Initialize features.
+    val feature_map =
+      init_set(init_map) >>
+        exprClassFeature >>
+        nameFeature
 
-      System.err.println("* data")
-      decls.foreach(decl => {
-        calls.foreach(call => {
-          val bitvectors = feature_map((decl, call))
-          bitvectors.foreach(v => System.out.print(v + " "))
-          System.out.print(":")
-          val answer = result_map((decl, call))
-          System.out.println(answer)
-        })
+    System.err.println("* data")
+    decls.foreach(decl => {
+      calls.foreach(call => {
+        val bitvectors = feature_map((decl, call))
+        bitvectors.foreach(v => System.out.print(v + " "))
+        System.out.print(":")
+        val answer = result_map((decl, call))
+        System.out.println(answer)
       })
-    }
+    })
 
     return_code
   }
@@ -259,190 +277,234 @@ object AnalyzeMain {
     def >>[B](f: A=>B) = f(a)
   }
 
-  def walkAST(parent: Any, node: Any)(pair: (List[Any], List[Any])): (List[Any], List[Any]) = node match {
-    case SProgram(info, body) =>
-      walkAST(node, body)(pair)
-    case SNoOp(info, desc) =>
-      pair
-    case SStmtUnit(info, stmts) =>
-      walkAST(node, stmts)(pair)
-    case SFunDecl(info, ftn, strict) =>
-      val new_pair = (node::pair._1, pair._2)
-      walkAST(node, ftn)(new_pair)
-    case SBlock(info, stmts, internal) =>
-      walkAST(node, stmts)(pair)
-    case SVarStmt(info, vds) =>
-      walkAST(node, vds)(pair)
-    case SEmptyStmt(info) =>
-      pair
-    case SExprStmt(info, expr, internal) =>
-      walkAST(node, expr)(pair)
-    case SIf(info, cond, trueBranch, falseBranch) =>
-      walkAST(node, cond)(pair) >>
-        walkAST(node, trueBranch) >>
-        walkAST(node, falseBranch)
-    case SDoWhile(info, body, cond) =>
-      walkAST(node, body)(pair) >>
-        walkAST(node, cond)
-    case SWhile(info, cond, body) =>
-      walkAST(node, cond)(pair) >>
-        walkAST(node, body)
-    case SFor(info, init, cond, action, body) =>
-      walkAST(node, init)(pair) >>
-        walkAST(node, cond) >>
-        walkAST(node, action) >>
-        walkAST(node, body)
-    case SForIn(info, lhs, expr, body) =>
-      walkAST(node, lhs)(pair) >>
-        walkAST(node, expr) >>
-        walkAST(node, body)
-    case SForVar(info, vars, cond, action, body) =>
-      walkAST(node, vars)(pair) >>
-        walkAST(node, cond) >>
-        walkAST(node, action) >>
-        walkAST(node, body)
-    case SForVarIn(info, _var, expr, body) =>
-      walkAST(node, _var)(pair) >>
-        walkAST(node, expr) >>
-        walkAST(node, body)
-    case SContinue(info, target) =>
-      walkAST(node, target)(pair)
-    case SBreak(info, target) =>
-      walkAST(node, target)(pair)
-    case SReturn(info, expr) =>
-      walkAST(node, expr)(pair)
-    case SWith(info, expr, stmt) =>
-      walkAST(node, expr)(pair) >>
-        walkAST(node, stmt)
-    case SSwitch(info, cond, frontCases, _def, backCases) =>
-      walkAST(node, cond)(pair) >>
-        walkAST(node, frontCases) >>
-        walkAST(node, _def) >>
-        walkAST(node, backCases)
-    case SLabelStmt(info, label, stmt) =>
-      walkAST(node, label)(pair) >>
-        walkAST(node, stmt)
-    case SThrow(info, expr) =>
-      walkAST(node, expr)(pair)
-    case STry(info, body, catchBlock, fin) =>
-      walkAST(node, body)(pair) >>
-        walkAST(node, catchBlock) >>
-        walkAST(node, fin)
-    case SDebugger(info) =>
-      pair
-    case SSourceElements(info, body, strict) =>
-      walkAST(node, body)(pair)
-    case SVarDecl(info, id, expr, strict) =>
-      walkAST(node, id)(pair) >>
-        walkAST(node, expr)
-    case SCase(info, cond, body) =>
-      walkAST(node, cond)(pair) >>
-        walkAST(node, body)
-    case SCatch(info, id, body) =>
-      walkAST(node, id)(pair) >>
-        walkAST(node, body)
-    case SExprList(info, exprs) =>
-      walkAST(node, exprs)(pair)
-    case SCond(info, cond, trueBranch, falseBranch) =>
-      walkAST(node, cond)(pair) >>
-        walkAST(node, trueBranch) >>
-        walkAST(node, falseBranch)
-    case SInfixOpApp(info, left, op, right) =>
-      walkAST(node, left)(pair) >>
-        walkAST(node, op) >>
-        walkAST(node, right)
-    case SPrefixOpApp(info, op, right) =>
-      walkAST(node, op)(pair) >>
-        walkAST(node, right)
-    case SUnaryAssignOpApp(info, lhs, op) =>
-      walkAST(node, lhs)(pair) >>
-        walkAST(node, op)
-    case SAssignOpApp(info, lhs, op, right) =>
-      walkAST(node, lhs)(pair) >>
-        walkAST(node, op) >>
-        walkAST(node, right)
-    case SThis(info) =>
-      pair
-    case SNull(info) =>
-      pair
-    case SBool(info, bool) =>
-      pair
-    case SDoubleLiteral(info, text, num) =>
-      pair
-    case SIntLiteral(info, intVal, radix) =>
-      pair
-    case SStringLiteral(info, quote, escaped) =>
-      pair
-    case SRegularExpression(info, body, flag) =>
-      pair
-    case SVarRef(info, id) =>
-      walkAST(node, id)(pair)
-    case SArrayExpr(info, elements) =>
-      walkAST(node, elements)(pair)
-    case SArrayNumberExpr(info, elements) =>
-      walkAST(node, elements)(pair)
-    case SObjectExpr(info, members) =>
-      walkAST(node, members)(pair)
-    case SParenthesized(info, expr) =>
-      walkAST(node, expr)(pair)
-    case SFunExpr(info, ftn) =>
-      val new_pair = (node::pair._1, pair._2)
-      walkAST(node, ftn)(new_pair)
-    case SBracket(info, obj, index) =>
-      walkAST(node, obj)(pair) >>
-        walkAST(node, index)
-    case SDot(info, obj, member) =>
-      walkAST(node, obj)(pair) >>
-        walkAST(node, member)
-    case SNew(info, lhs) =>
-      val new_pair =
-        lhs match {
-          case SFunApp(_, _, _) => pair
-          case _ => (pair._1, node::pair._2) // case for 'new A'
-        }
-      walkAST(node, lhs)(new_pair)
-    case SFunApp(info, fun, args) =>
-      val new_pair = (pair._1, node::pair._2)
-      walkAST(node, fun)(new_pair) >>
-        walkAST(node, args)
-    case SPropId(info, id) =>
-      walkAST(node, id)(pair)
-    case SPropStr(info, str) =>
-      pair
-    case SPropNum(info, num) =>
-      pair
-    case SField(info, prop, expr) =>
-      walkAST(node, prop)(pair) >>
-        walkAST(node, expr)
-    case SGetProp(info, prop, ftn) =>
-      walkAST(node, prop)(pair) >>
-        walkAST(node, ftn)
-    case SSetProp(info, prop, ftn) =>
-      walkAST(node, prop)(pair) >>
-        walkAST(node, ftn)
-    case SId(info, text, uniqueName, _with) =>
-      pair
-    case SOp(info, text) =>
-      pair
-    case SLabel(info, id) =>
-      walkAST(node, id)(pair)
-    case SComment(info, comment) =>
-      pair
-    case STopLevel(fds, vds, stmts) =>
-      walkAST(node, fds)(pair) >>
-        walkAST(node, vds) >>
-        walkAST(node, stmts)
-    case SFunctional(fds, vds, stmts, id, params) =>
-      walkAST(node, fds)(pair) >>
-        walkAST(node, vds) >>
-        walkAST(node, stmts) >>
-        walkAST(node, id) >>
-        walkAST(node, params)
-    case list: List[_] =>
-      list.foldLeft(pair)((p, node) => walkAST(parent, node)(p))
-    case Some(n) =>
-      walkAST(parent, n)(pair)
-    case None =>
-      pair
+  def walkAST[A](f: (Any, Any, A) => A)(parent: Any, node: Any)(a: A): A = {
+    val rec = walkAST(f)(_,_)
+    node match {
+      case SProgram(info, body) =>
+        f(parent, node, a) >>
+          rec(node, body)
+      case SNoOp(info, desc) =>
+        f(parent, node, a)
+      case SStmtUnit(info, stmts) =>
+        f(parent, node, a) >>
+          rec(node, stmts)
+      case SFunDecl(info, ftn, strict) =>
+        f(parent, node, a) >>
+          rec(node, ftn)
+      case SBlock(info, stmts, internal) =>
+        f(parent, node, a) >>
+          rec(node, stmts)
+      case SVarStmt(info, vds) =>
+        f(parent, node, a) >>
+          rec(node, vds)
+      case SEmptyStmt(info) =>
+        f(parent, node, a)
+      case SExprStmt(info, expr, internal) =>
+        f(parent, node, a) >>
+          rec(node, expr)
+      case SIf(info, cond, trueBranch, falseBranch) =>
+        f(parent, node, a) >>
+          rec(node, cond) >>
+          rec(node, trueBranch) >>
+          rec(node, falseBranch)
+      case SDoWhile(info, body, cond) =>
+        f(parent, node, a) >>
+          rec(node, body) >>
+          rec(node, cond)
+      case SWhile(info, cond, body) =>
+        f(parent, node, a) >>
+          rec(node, cond) >>
+          rec(node, body)
+      case SFor(info, init, cond, action, body) =>
+        f(parent, node, a) >>
+          rec(node, init) >>
+          rec(node, cond) >>
+          rec(node, action) >>
+          rec(node, body)
+      case SForIn(info, lhs, expr, body) =>
+        f(parent, node, a) >>
+          rec(node, lhs) >>
+          rec(node, expr) >>
+          rec(node, body)
+      case SForVar(info, vars, cond, action, body) =>
+        f(parent, node, a) >>
+          rec(node, vars) >>
+          rec(node, cond) >>
+          rec(node, action) >>
+          rec(node, body)
+      case SForVarIn(info, _var, expr, body) =>
+        f(parent, node, a) >>
+          rec(node, _var) >>
+          rec(node, expr) >>
+          rec(node, body)
+      case SContinue(info, target) =>
+        f(parent, node, a) >>
+          rec(node, target)
+      case SBreak(info, target) =>
+        f(parent, node, a) >>
+          rec(node, target)
+      case SReturn(info, expr) =>
+        f(parent, node, a) >>
+          rec(node, expr)
+      case SWith(info, expr, stmt) =>
+        f(parent, node, a) >>
+          rec(node, expr) >>
+          rec(node, stmt)
+      case SSwitch(info, cond, frontCases, _def, backCases) =>
+        f(parent, node, a) >>
+          rec(node, cond) >>
+          rec(node, frontCases) >>
+          rec(node, _def) >>
+          rec(node, backCases)
+      case SLabelStmt(info, label, stmt) =>
+        f(parent, node, a) >>
+          rec(node, label) >>
+          rec(node, stmt)
+      case SThrow(info, expr) =>
+        f(parent, node, a) >>
+          rec(node, expr)
+      case STry(info, body, catchBlock, fin) =>
+        f(parent, node, a) >>
+          rec(node, body) >>
+          rec(node, catchBlock) >>
+          rec(node, fin)
+      case SDebugger(info) =>
+        f(parent, node, a)
+      case SSourceElements(info, body, strict) =>
+        f(parent, node, a) >>
+          rec(node, body)
+      case SVarDecl(info, id, expr, strict) =>
+        f(parent, node, a) >>
+          rec(node, id) >>
+          rec(node, expr)
+      case SCase(info, cond, body) =>
+        f(parent, node, a) >>
+          rec(node, cond) >>
+          rec(node, body)
+      case SCatch(info, id, body) =>
+        f(parent, node, a) >>
+          rec(node, id) >>
+          rec(node, body)
+      case SExprList(info, exprs) =>
+        f(parent, node, a) >>
+          rec(node, exprs)
+      case SCond(info, cond, trueBranch, falseBranch) =>
+        f(parent, node, a) >>
+          rec(node, cond) >>
+          rec(node, trueBranch) >>
+          rec(node, falseBranch)
+      case SInfixOpApp(info, left, op, right) =>
+        f(parent, node, a) >>
+          rec(node, left) >>
+          rec(node, op) >>
+          rec(node, right)
+      case SPrefixOpApp(info, op, right) =>
+        f(parent, node, a) >>
+          rec(node, op) >>
+          rec(node, right)
+      case SUnaryAssignOpApp(info, lhs, op) =>
+        f(parent, node, a) >>
+          rec(node, lhs) >>
+          rec(node, op)
+      case SAssignOpApp(info, lhs, op, right) =>
+        f(parent, node, a) >>
+          rec(node, lhs) >>
+          rec(node, op) >>
+          rec(node, right)
+      case SThis(info) =>
+        f(parent, node, a)
+      case SNull(info) =>
+        f(parent, node, a)
+      case SBool(info, bool) =>
+        f(parent, node, a)
+      case SDoubleLiteral(info, text, num) =>
+        f(parent, node, a)
+      case SIntLiteral(info, intVal, radix) =>
+        f(parent, node, a)
+      case SStringLiteral(info, quote, escaped) =>
+        f(parent, node, a)
+      case SRegularExpression(info, body, flag) =>
+        f(parent, node, a)
+      case SVarRef(info, id) =>
+        f(parent, node, a) >>
+          rec(node, id)
+      case SArrayExpr(info, elements) =>
+        f(parent, node, a) >>
+          rec(node, elements)
+      case SArrayNumberExpr(info, elements) =>
+        f(parent, node, a) >>
+          rec(node, elements)
+      case SObjectExpr(info, members) =>
+        f(parent, node, a) >>
+          rec(node, members)
+      case SParenthesized(info, expr) =>
+        f(parent, node, a) >>
+          rec(node, expr)
+      case SFunExpr(info, ftn) =>
+        f(parent, node, a) >>
+          rec(node, ftn)
+      case SBracket(info, obj, index) =>
+        f(parent, node, a) >>
+          rec(node, obj) >>
+          rec(node, index)
+      case SDot(info, obj, member) =>
+        f(parent, node, a) >>
+          rec(node, obj) >>
+          rec(node, member)
+      case SNew(info, lhs) =>
+        f(parent, node, a) >>
+          rec(node, lhs)
+      case SFunApp(info, fun, args) =>
+        f(parent, node, a) >>
+          rec(node, fun) >>
+          rec(node, args)
+      case SPropId(info, id) =>
+        f(parent, node, a) >>
+          rec(node, id)
+      case SPropStr(info, str) =>
+        f(parent, node, a)
+      case SPropNum(info, num) =>
+        f(parent, node, a)
+      case SField(info, prop, expr) =>
+        f(parent, node, a) >>
+          rec(node, prop) >>
+          rec(node, expr)
+      case SGetProp(info, prop, ftn) =>
+        f(parent, node, a) >>
+          rec(node, prop) >>
+          rec(node, ftn)
+      case SSetProp(info, prop, ftn) =>
+        f(parent, node, a) >>
+          rec(node, prop) >>
+          rec(node, ftn)
+      case SId(info, text, uniqueName, _with) =>
+        f(parent, node, a)
+      case SOp(info, text) =>
+        f(parent, node, a)
+      case SLabel(info, id) =>
+        f(parent, node, a) >>
+          rec(node, id)
+      case SComment(info, comment) =>
+        f(parent, node, a)
+      case STopLevel(fds, vds, stmts) =>
+        f(parent, node, a) >>
+          rec(node, fds) >>
+          rec(node, vds) >>
+          rec(node, stmts)
+      case SFunctional(fds, vds, stmts, id, params) =>
+        f(parent, node, a) >>
+          rec(node, fds) >>
+          rec(node, vds) >>
+          rec(node, stmts) >>
+          rec(node, id) >>
+          rec(node, params)
+      case list: List[_] =>
+        list.foldLeft(a)((b, node) => rec(parent, node)(b))
+      case Some(n) =>
+        f(parent, node, a) >>
+          rec(parent, n)
+      case None =>
+        f(parent, node, a)
+    }
   }
 }
